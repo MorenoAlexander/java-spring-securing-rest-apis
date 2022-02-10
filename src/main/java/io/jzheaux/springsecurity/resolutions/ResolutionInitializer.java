19c47ebd4b1d9dab0d410359a69c0a8a4d8d4a92
Alexander Moreno <am@alexandermoreno.com>
2022-02-10 01:11:26 -0600

    added UserRepository, Users, Add Users to Database

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -8,15 +8,23 @@ import java.util.UUID;
 @Component
 public class ResolutionInitializer implements SmartInitializingSingleton {
 	private final ResolutionRepository resolutions;
+	private final UserRepository users;
 
-	public ResolutionInitializer(ResolutionRepository resolutions) {
+
+	public ResolutionInitializer(ResolutionRepository resolutions, UserRepository users) {
 		this.resolutions = resolutions;
+		this.users = users;
 	}
+
+
 
 	@Override
 	public void afterSingletonsInstantiated() {
 		this.resolutions.save(new Resolution("Read War and Peace", "user"));
 		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
 		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+
+		User user = new User("user", "test123");
+		this.users.save(user);
 	}
 }
